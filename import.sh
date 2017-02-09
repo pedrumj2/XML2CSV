0004868016f4f69e24d87b39f541cd8d4a4e2f29
pedrum <pedrum.jalali3@gmail.com>
2017-02-08 21:05:52 -0600

    f

--- import.sh
+++ import.sh
@@ -2,8 +2,9 @@
 
 
 cat headers.txt > CSV
-eval "xsltproc transform.xsl $1 >> CSV"
-eval "sed '2,3d' CSV > CSV2"
+eval "xsltproc transform.xsl $1 >> CSV2"
+eval "sed '2,3d' CSV2 > CSV"
+eval "rm CSV2"
 
 
 
